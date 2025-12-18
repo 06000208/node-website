412af03fee710300579913de2965db5cbc61ef73
amber <a06000208@protonmail.com>
2025-12-18 12:52:00 -0600

    Environment variable handling

---
+++ src/environment.js
@@ -0,0 +1,12 @@
+// Intermediary module directly exporting environment variables for extra functionality, intellisense, and consistent cross-platform use.
+
+// When providing node.js with environment variables, including via the .env file, they should follow google's naming standard and use SCREAMING_SNAKE_CASE for names.
+// https://google.github.io/styleguide/shellguide.html#s7.3-constants-and-environment-variable-names
+// https://web.archive.org/web/20220415192041id_/https://google.github.io/styleguide/shellguide.html#s7.3-constants-and-environment-variable-names
+
+import { env } from "node:process";
+import { stringToBoolean } from "./utility.js";
+import { environmentVariableNames as names } from "./constants.js";
+
+export const development = stringToBoolean(env[names.development]);
+export const production = !development;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+// This file is the entry point for the project and controls load order
+// The order of import statements matters as they're loaded asynchronously
+
+import "./scripts/env.js"; // Populates environment variables
